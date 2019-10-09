3733d55a14dd2f4dbaffa01d1a3d1580f0d10af4
lzhao <lzhao@LAPTOP-69L2HK7H.3520.dhcp.asu.edu>
2019-10-09 10:30:45 -0700

    Added method for calculator function(add, subtract, print)

--- cse360Assign2/src/cse360Assign2/AddingMachine.java
+++ cse360Assign2/src/cse360Assign2/AddingMachine.java
@@ -2,29 +2,63 @@ package cse360Assign2;
 
 public class AddingMachine 
 {
-private int total;
+	private int total = 0;
+	private String str = "0";
 	
-	public AddingMachine () {
-		total = 0;  // not needed - included for clarity
-	}
+		public AddingMachine () 
+		{
+			  // not needed - included for clarity
+		}
 	
-	public int getTotal () {
-		return 0;
-	}
+		public int getTotal () 
+		{
+			return this.total;
+		}
 	
-	public void add (int value) {
+		public void add (int value) 
+		{
+			this.total += value;
+			this.str += "+" + value;
+		}
+	
+		public void subtract (int value) 
+		{
+			this.total -= value;
+			this.str += "-" + value;
+		}
 		
-	}
-	
-	public void subtract (int value) {
-		
-	}
-		
-	public String toString () {
-		return "";
-	}
+		public String toString () 
+		{
+			return this.str;
+		}
 
-	public void clear() {
-	
-	}
+		public void clear() 
+		{
+			this.total = 0;
+			this.str = "0";
+		}
+		
+		public void setTotal(int total) {
+	        this.total = total;
+	    }
+
+	    public String getStr() 
+	    {
+	        return str;
+	    }
+
+	    public void setProcessString(String str) 
+	    {
+	        this.str = str;
+	    }
+
+	    public static void main(String[] args)
+	    {
+	        AddingMachine myCalc = new AddingMachine();
+	        myCalc.add (5);
+	        myCalc.subtract (2);
+	        myCalc.add(5);
+	        System.out.println(myCalc);
+	        System.out.println(myCalc.getTotal());
+	    }
 }
